0db8c07655dc187e7bcb164a59f9fe7141d33049
ranjit <ranjithkumar2010a@live.com>
2017-08-28 18:04:24 +0530

    Initial Commit

---
+++ FirebasePhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebasePhone
+//
+//  Created by Ranjith Kumar on 8/28/17.
+//  Copyright © 2017 Ranjith Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
